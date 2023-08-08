b934e4bbebd07fa189db9f6f5f0732277ebf92e2
Jatin Garg <jgarg8440@gmail.com>
2023-08-09 00:54:50 +0530

    first commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/portfoliosuggestion").then(() =>{
+mongoose.connect("mongodb+srv://jgarg8440:t9Uz2SNEqapdmwLf@cluster0.ybnd1gg.mongodb.net/?retryWrites=true&w=majority").then(() =>{
     console.log(`Connection successful`);
 }).catch((e) =>{
     console.log(`no connection`);
